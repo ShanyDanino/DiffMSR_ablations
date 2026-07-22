7469d6597343131150a93606cf9008cab9891862
ShanyDanino <shanyd14@gmail.com>
2026-07-22 23:15:12 +0300

    yamls for image space

---
+++ options/pd_fs_ablation/test/test_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONPATH="$(pwd)"
+python3  DiffMSR_Main/test.py -opt options/image/test_DiffMSR_S2_image.yml
---
+++ options/pd_fs_ablation/train/train_S1_image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export PYTHONPATH="$(pwd)"
+CUDA_VISIBLE_DEVICES=0 python DiffMSR_Main/train.py -opt options/image/train_DiffMSR_S1_x4_image.yml
